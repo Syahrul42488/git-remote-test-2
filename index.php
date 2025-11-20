7be5b369a113aea4430db17fbbe35a3ff6342a87
Syahrul Mun'im <syahrulmunim070905@gmail.com>
2025-11-20 22:08:48 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti exercitationem eveniet magni neque dignissimos incidunt officia facilis perspiciatis ipsum pariatur praesentium enim, ut numquam facere inventore eaque laborum explicabo similique!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
